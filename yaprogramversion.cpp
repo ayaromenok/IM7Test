07411cdf708337e7c1c57d51d1ae8f1d10d3d940
Andrey Yaromenok <ayaromenok@gmail.com>
2017-10-30 13:23:50 +0100

    #9 clear program_version code, also fix a bug with displaying current branch

--- yaprogramversion.cpp
+++ yaprogramversion.cpp
@@ -5,22 +5,27 @@
 
 YaProgramVersion::YaProgramVersion(QObject *parent) : QObject(parent)
 {
-    QFile file(":/program_version.txt");
+    QFile file(":/prj/program_version.txt");
     if (file.open(QIODevice::ReadOnly)){
         QTextStream qts(&file);
-        quint32 line = 0;
-        while(!qts.atEnd()) {
-            if (line == 0)
-                _tag.append(qts.readLine());
-            else if (line == 1)
-                _count.append(qts.readLine());
-            else if (line == 2)
-                _branch.append(qts.readLine());
-            else {
-                _lastCommit.append(qts.readLine());
-                _lastCommit.append(" ");
-            }
-            line++;
+        QString strVerInfo(qts.readAll());
+
+        QStringList strListVerInfo(strVerInfo.split('\n'));
+
+        _tag.append(strListVerInfo.at(0));
+        _count.append(strListVerInfo.at(1));
+
+        foreach (const QString &str, strListVerInfo) {
+            if (str.contains("*"))
+                _branch += str;
+            if (str.contains("commit"))
+                _lastCommit += str;
+            if (str.contains("Author:"))
+                _lastCommit += str;
+            if (str.contains("Date"))
+                _lastCommit += str;
+            if (str.contains("   #"))//assume that every commit belongs to issue
+                _lastCommit += str;
         }
     }
 }
